197cf9d42dfc165bdfa88e6ce62aa217dd83bc8b
Rui Monteiro <ruiseixas@yahoo.com>
2025-11-19 15:47:47 +0000

    Cleaned old files

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "include_lib.h"
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World!" << std::endl;
-    IncludeLib *lib_include = new IncludeLib();
-    lib_include->PrintText();
-    return 0;
-}
