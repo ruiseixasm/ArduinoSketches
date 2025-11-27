b39f62a5eff8bd733382bb6ebc29296d154dc24b
Rui Monteiro <ruiseixas@yahoo.com>
2025-11-27 23:36:12 +0000

    Added String version

---
+++ SPI_Wire/NanoNano_String/NanoMaster/NanoMaster.ino
@@ -0,0 +1,62 @@
+// SPI Master Code - Pure String Commands
+#include <SPI.h>
+
+// Pin definitions
+const int BUZZ_PIN = 2; // External BUZZER pin
+const int SS_PIN = 10;  // Slave Select pin
+
+void setup() {
+  // Initialize SPI
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV16);
+  SPI.setDataMode(SPI_MODE0);
+  
+  // Initialize pins
+  pinMode(SS_PIN, OUTPUT);
+  digitalWrite(SS_PIN, HIGH);
+  pinMode(BUZZ_PIN, OUTPUT);
+  digitalWrite(BUZZ_PIN, LOW);
+  
+  // Initialize serial
+  Serial.begin(115200);
+  delay(500);
+  Serial.println("/n/nSPI Master Initialized - String Mode");
+}
+
+void loop() {
+  // Send string commands directly
+  sendString("LED_ON");
+  delay(2000);
+  
+  sendString("LED_OFF");
+  delay(2000);
+}
+
+void sendString(const char* command) {
+  // Select slave
+  digitalWrite(SS_PIN, LOW);
+  delayMicroseconds(100);
+  
+  // Send each character of the string
+  int i = 0;
+  while (command[i] != '\0') {
+    SPI.transfer(command[i]);
+    i++;
+    delayMicroseconds(10);
+  }
+  
+  // Send null terminator to mark end of string
+  SPI.transfer('\0');
+  
+  // Deselect slave
+  digitalWrite(SS_PIN, HIGH);
+  
+  Serial.print("Sent: ");
+  Serial.println(command);
+  
+  // Buzz feedback
+  digitalWrite(BUZZ_PIN, HIGH);
+  delay(5);
+  digitalWrite(BUZZ_PIN, LOW);
+}
+
---
+++ SPI_Wire/NanoNano_String/NanoSlave/NanoSlave.ino
@@ -0,0 +1,65 @@
+// SPI Slave Code - Pure String Commands
+#include <SPI.h>
+
+// Pin definitions
+const int LED_PIN = 2;
+const int SS_PIN = 10;
+
+#define BUFFER_SIZE 32
+char commandBuffer[BUFFER_SIZE];
+byte bufferIndex = 0;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  
+  // Initialize SPI as slave
+  SPCR |= _BV(SPE);
+  SPCR |= _BV(SPIE);
+  SPI.attachInterrupt();
+  
+  Serial.begin(115200);
+  delay(500);
+  Serial.println("/n/nSPI Slave Ready - String Mode");
+}
+
+// SPI interrupt - receive characters
+ISR (SPI_STC_vect) {
+  char receivedChar = SPDR;
+  
+  if (receivedChar == '\0') {
+    // End of string - process command
+    commandBuffer[bufferIndex] = '\0';
+    bufferIndex = 0;
+    
+    // Process the complete command
+    if (strlen(commandBuffer) > 0) {
+      processCommand(commandBuffer);
+    }
+  } else if (bufferIndex < BUFFER_SIZE - 1) {
+    // Store character in buffer
+    commandBuffer[bufferIndex++] = receivedChar;
+  }
+}
+
+void processCommand(const char* command) {
+  Serial.print("Received: ");
+  Serial.println(command);
+  
+  if (strcmp(command, "LED_ON") == 0) {
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("LED turned ON");
+  }
+  else if (strcmp(command, "LED_OFF") == 0) {
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("LED turned OFF");
+  }
+  else {
+    Serial.println("Unknown command");
+  }
+}
+
+void loop() {
+  // Nothing needed here - everything happens in ISR
+}
+
