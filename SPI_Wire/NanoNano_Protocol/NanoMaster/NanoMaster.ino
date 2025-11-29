ea3ba4006d1558472e65c7deb902260a4f6ad161
Rui Monteiro <ruiseixas@yahoo.com>
2025-11-29 01:46:45 +0000

    Added the Protocol version

---
+++ SPI_Wire/NanoNano_Protocol/NanoMaster/NanoMaster.ino
@@ -0,0 +1,115 @@
+// SPI Master Code - Pure String Commands
+#include <SPI.h>
+
+
+// How to Use Sigrok PulseView Software | Logic Analyzer
+//     https://sigrok.org/wiki/Downloads
+//     Saleae Logic - 24MHz - 8CH - fx2lafw (generic driver for FX2 based LAs) (fx2lafw)
+//     Capture: 5M samples @ 12MHz
+
+
+enum MessageCode : uint8_t {
+    START   = 0xF0, // Start of transmission
+    END     = 0xF1, // End of transmission
+    ACK     = 0xF2, // Acknowledge
+    NACK    = 0xF3, // Not acknowledged
+    READY   = 0xF4, // Slave has response ready
+    ERROR   = 0xF5  // Error frame
+};
+
+
+// Pin definitions
+const int BUZZ_PIN = 2; // External BUZZER pin
+const int SS_PIN = 10;  // Slave Select pin
+
+
+void setup() {
+
+    // SPI_CLOCK_DIV2 = 8MHz (Arduino Nano 16MHz / 2)
+    // SPI_CLOCK_DIV4 = 4MHz (Arduino Nano 16MHz / 4) ← This is what you want
+    // SPI_CLOCK_DIV8 = 2MHz (Arduino Nano 16MHz / 8)
+    // SPI_CLOCK_DIV16 = 1MHz (Arduino Nano 16MHz / 16) ← What you had
+
+    // Initialize SPI
+    SPI.begin();
+    SPI.setClockDivider(SPI_CLOCK_DIV4);    // Only affects the char transmission
+    SPI.setDataMode(SPI_MODE0);
+    SPI.setBitOrder(MSBFIRST);  // EXPLICITLY SET MSB FIRST! (OTHERWISE is LSB)
+    
+    // Initialize pins
+    pinMode(SS_PIN, OUTPUT);
+    digitalWrite(SS_PIN, HIGH);
+    pinMode(BUZZ_PIN, OUTPUT);
+    digitalWrite(BUZZ_PIN, LOW);
+    
+    // Initialize serial
+    Serial.begin(115200);
+    delay(500);
+    Serial.println("\n\nSPI Master Initialized - One Way Communication");
+}
+
+void loop() {
+    // Send string commands directly
+    sendString("LED_ON");
+    delay(2000);
+    
+    sendString("LED_OFF");
+    delay(2000);
+}
+
+#define micro_delay 9
+
+bool sendString(const char* command) {
+    
+    // WARNING:
+    //     AVOID PLACING Serial.print CALLS HERE BECAUSE IT WILL DELAY 
+    //     THE POSSIBILITY OF SPI CAPTURE AND RESPONSE IN TIME !!!
+
+    // char is signed by default on most Arduino platforms (-128 to +127)
+    // char c; // DON'T USE char BECAUSE BECOMES SIGNED!!
+    uint8_t c; // Always able to receive (FULL DUPLEX)
+    bool successfully_sent = false;
+
+    for (size_t s = 0; !successfully_sent && s < 3; s++) {
+  
+        successfully_sent = true;
+
+        digitalWrite(SS_PIN, LOW);
+        delayMicroseconds(5);
+
+        // Signals the start of the transmission
+        SPI.transfer(START);
+        delayMicroseconds(1);
+        
+        // Send command
+        int i = 0;
+        while (command[i] != '\0') {
+            SPI.transfer(command[i]);
+            i++;
+            delayMicroseconds(micro_delay);
+        }
+        SPI.transfer('\0');
+        delayMicroseconds(5);   // It has to process '\0' as a common char
+
+        // Signals the end of the transmission
+        if (SPI.transfer(END) != ACK)
+            successfully_sent = false;
+        
+        delayMicroseconds(1);
+        digitalWrite(SS_PIN, HIGH);
+
+        if (successfully_sent) {
+            Serial.println("Command successfully sent");
+        } else {
+            // digitalWrite(BUZZ_PIN, HIGH);
+            // delay(30);  // Buzzer on for 30ms
+            // digitalWrite(BUZZ_PIN, LOW);
+            Serial.print("Command NOT successfully sent on try: ");
+            Serial.println(s + 1);
+            Serial.println("BUZZER activated for 30ms!");
+        }
+    }
+
+    return successfully_sent;
+}
+
---
+++ SPI_Wire/NanoNano_Protocol/NanoSlave/NanoSlave.ino
@@ -0,0 +1,132 @@
+// SPI Slave Code - Pure String Commands
+#include <SPI.h>
+
+
+enum MessageCode : uint8_t {
+    START   = 0xF0, // Start of transmission
+    END     = 0xF1, // End of transmission
+    ACK     = 0xF2, // Acknowledge
+    NACK    = 0xF3, // Not acknowledged
+    READY   = 0xF4, // Slave has response ready
+    ERROR   = 0xF5  // Error frame
+};
+
+
+// Pin definitions
+const int LED_PIN = 2;
+const int SS_PIN = 10;
+
+#define BUFFER_SIZE 128
+char receiving_buffer[BUFFER_SIZE];
+volatile byte receiving_index = 0;
+volatile bool receiving_state = false;
+
+// For loop flags
+volatile bool process_command = false;
+
+
+void setup() {
+    Serial.begin(115200);
+    delay(500);
+    Serial.println("\n\nSPI Slave Ready - One Way Communication");
+
+    
+    Serial.print("LED_PIN: ");
+    Serial.println(LED_PIN);
+
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
+
+    pinMode(MISO, OUTPUT);  // MISO must be OUTPUT for Slave to send data!
+
+    // Initialize SPI as slave - EXPLICIT MSB FIRST
+    SPCR = 0;  // Clear register
+    SPCR |= _BV(SPE);    // SPI Enable
+    SPCR |= _BV(SPIE);   // SPI Interrupt Enable  
+    SPCR &= ~_BV(DORD);  // MSB First (DORD=0 for MSB first)
+    SPCR &= ~_BV(CPOL);  // Clock polarity 0
+    SPCR &= ~_BV(CPHA);  // Clock phase 0 (MODE0)
+
+    // // Initialize SPI as slave
+    // SPCR |= _BV(SPE);
+    // SPCR |= _BV(SPIE);
+
+
+    SPI.attachInterrupt();
+
+}
+
+
+// ------------------------------
+// SPI Interrupt
+// ------------------------------
+ISR(SPI_STC_vect) {
+
+    // AVOID PLACING HEAVY CODE OR CALL HERE. THIS INTERRUPTS THE LOOP!
+
+    // WARNING:
+    //     AVOID PLACING Serial.print CALLS HERE BECAUSE IT WILL DELAY 
+    //     THE POSSIBILITY OF SPI CAPTURE AND RESPONSE IN TIME !!!
+
+    // char is signed by default on most Arduino platforms (-128 to +127)
+    // char c = SPDR;    // DON'T USE char BECAUSE BECOMES SIGNED!!
+    uint8_t c = SPDR;    // The most important line!!
+
+    // Serial.print("0. Slave received: 0x");
+    // Serial.println(c, HEX);  // Debug what byte actually arrives
+    
+    if (c == START) {
+        // Serial.println("1. Start receiving");
+        receiving_state = true;
+        receiving_index = 0;
+        SPDR = ACK;  // Send acknowledgment back to Master
+    } else if (c == END) {
+        // Serial.println("2. End receiving");
+        receiving_state = false;
+        if (receiving_index > 0) {
+            process_command = true;
+            SPDR = ACK;     // Send acknowledgment back to Master
+        } else {
+            SPDR = ERROR;   // No chars received
+        }
+    } else if (receiving_state) {
+
+        if (receiving_index < BUFFER_SIZE) {
+            receiving_buffer[receiving_index++] = c;
+        } else {
+            receiving_state = false;
+        }
+        SPDR = ACK;     // Send acknowledgment back to Master
+    } else {
+        SPDR = ERROR;
+    }
+}
+
+
+void processCommand() {
+
+    Serial.print("Processed command: ");
+    Serial.println(receiving_buffer);
+
+    if (strcmp(receiving_buffer, "LED_ON") == 0) {
+        digitalWrite(LED_PIN, HIGH);
+        Serial.println("LED is ON");
+    }
+    else if (strcmp(receiving_buffer, "LED_OFF") == 0) {
+        digitalWrite(LED_PIN, LOW);
+        Serial.println("LED is OFF");
+    }
+    else {
+        Serial.println("Unknown command");
+    }
+}
+
+void loop() {
+    // HEAVY PROCESSING SHALL BE IN THE LOOP
+
+    if (process_command) {
+        processCommand();   // Called only once!
+        process_command = false;    // Critical to avoid repeated calls over the ISR function
+    }
+}
+
