8a3aa732360f2a46ec30699fc63d01dce35407b7
Rui Monteiro <ruiseixas@yahoo.com>
2025-11-30 08:57:34 +0000

    Adjusted BroadcastSocket_SPI_Master class

--- JsonMidiCreator/TalkieSPI/src/sockets/BroadcastSocket_SPI_Master.hpp
+++ JsonMidiCreator/TalkieSPI/src/sockets/BroadcastSocket_SPI_Master.hpp
@@ -11,84 +11,40 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 Lesser General Public License for more details.
 https://github.com/ruiseixasm/JsonTalkie
 */
-#ifndef BROADCAST_SOCKET_ETHERNETENC_HPP
-#define BROADCAST_SOCKET_ETHERNETENC_HPP
-
-    // writeReg(ERXFCON, ERXFCON_UCEN | ERXFCON_CRCEN | ERXFCON_BCEN);
-
-    // // Pattern matching disabled – not needed for broadcast
-
-    // // writeRegPair(EPMM0, 0x303f);
-
-    // // writeRegPair(EPMCSL, 0xf7f9);
-
-#include "../Changed_EthernetENC/src/EthernetENC.h"
-// #include <EthernetUdp.h>  // DON'T INCLUDE THIS ONE BECAUSE ARDUINO COMPILER CAN PICH THE WRONG ONE
-#include "../Changed_EthernetENC/src/EthernetUdp.h"    // Forces the correct usage of EthernetUdp.h
+#ifndef BROADCAST_SOCKET_SPI_MASTER_HPP
+#define BROADCAST_SOCKET_SPI_MASTER_HPP
 
 
+#include <SPI.h>
+#include <ArduinoJson.h>    // Include ArduinoJson Library to be used as a dictionary
 #include "../BroadcastSocket.h"
 
-// #define BROADCAST_ETHERNETENC_DEBUG
+// #define BROADCAST_SPI_MASTER_DEBUG
 
 
 #define ENABLE_DIRECT_ADDRESSING
 
 
-class BroadcastSocket_EthernetENC : public BroadcastSocket {
+class BroadcastSocket_SPI_Master : public BroadcastSocket {
 private:
-    uint16_t _port = 5005;
-    IPAddress _source_ip = IPAddress(255, 255, 255, 255);   // By default it's used the broadcast IP
-    EthernetUDP* _udp = nullptr;
+
 
 protected:
     // Needed for the compiler, the base class is the one being called though
     // ADD THIS CONSTRUCTOR - it calls the base class constructor
-    BroadcastSocket_EthernetENC(JsonTalker** json_talkers, uint8_t talker_count)
+    BroadcastSocket_SPI_Master(JsonTalker** json_talkers, uint8_t talker_count)
         : BroadcastSocket(json_talkers, talker_count) {}
 
 
     size_t send(size_t length, bool as_reply = false) override {
-        if (_udp == nullptr) return false;
 
         // Need to call homologous method in super class first
         length = BroadcastSocket::send(length, as_reply); // Very important pre processing !!
 
         if (length > 0) {
 
-            IPAddress broadcastIP(255, 255, 255, 255);
             
-            #ifdef ENABLE_DIRECT_ADDRESSING
-            if (!_udp->beginPacket(as_reply ? _source_ip : broadcastIP, _port)) {
-                #ifdef BROADCAST_ETHERNETENC_DEBUG
-                Serial.println(F("Failed to begin packet"));
-                #endif
-                return 0;
-            }
-            #else
-            if (!_udp->beginPacket(broadcastIP, _port)) {
-                #ifdef BROADCAST_ETHERNETENC_DEBUG
-                Serial.println(F("Failed to begin packet"));
-                #endif
-                return 0;
-            }
-            #endif
 
-            size_t bytesSent = _udp->write(reinterpret_cast<const uint8_t*>(_sending_buffer), length);
-            (void)bytesSent; // Silence unused variable warning
-
-            if (!_udp->endPacket()) {
-                #ifdef BROADCAST_ETHERNETENC_DEBUG
-                Serial.println(F("Failed to end packet"));
-                #endif
-                return 0;
-            }
-
-            #ifdef BROADCAST_ETHERNETENC_DEBUG
-            Serial.print(F("B: "));
-            Serial.write(_sending_buffer, length);
-            Serial.println();
-            #endif
 
         }
 
@@ -99,50 +55,21 @@ protected:
 public:
 
     // Move ONLY the singleton instance method to subclass
-    static BroadcastSocket_EthernetENC& instance(JsonTalker** json_talkers, uint8_t talker_count) {
-        static BroadcastSocket_EthernetENC instance(json_talkers, talker_count);
+    static BroadcastSocket_SPI_Master& instance(JsonTalker** json_talkers, uint8_t talker_count) {
+        static BroadcastSocket_SPI_Master instance(json_talkers, talker_count);
         return instance;
     }
 
 
-    void set_port(uint16_t port) {
-        _port = port;
-    }
-    
 
     size_t receive() override {
-        if (_udp == nullptr) return 0;
 
         // Need to call homologous method in super class first
         BroadcastSocket::receive(); // Very important to do or else it may stop receiving !!
 
-        // Receive packets
-        int packetSize = _udp->parsePacket();
-        if (packetSize > 0) {
-
-            // Avoids overflow
-            if (packetSize > BROADCAST_SOCKET_BUFFER_SIZE) return 0;
-
-            int length = _udp->read(_receiving_buffer, static_cast<size_t>(packetSize));
-            if (length <= 0) return 0;  // Your requested check - handles all error cases
-            
-            #ifdef BROADCAST_ETHERNETENC_DEBUG
-            Serial.print(packetSize);
-            Serial.print(F("B from "));
-            Serial.print(_udp->remoteIP());
-            Serial.print(F(":"));
-            Serial.print(_udp->remotePort());
-            Serial.print(F(" -> "));
-            Serial.println(_receiving_buffer);
-            #endif
-            
-            _source_ip = _udp->remoteIP();
-            return triggerTalkers(static_cast<size_t>(length));
-        }
         return 0;   // nothing received
     }
 
-    void set_udp(EthernetUDP* udp) { _udp = udp; }
 };
 
-#endif // BROADCAST_SOCKET_ETHERNETENC_HPP
+#endif // BROADCAST_SOCKET_SPI_MASTER_HPP
