7edf8f173c5635d768218569c987f18a61f7516d
Rui Monteiro <ruiseixas@yahoo.com>
2025-11-30 04:06:56 +0000

    Replaced FullDuplex with JSON

---
+++ SPI_Wire/NanoNano_JSON/NanoMaster/NanoMaster.ino
@@ -0,0 +1,193 @@
+// SPI Master Code - Pure String Commands
+#include <SPI.h>
+
+
+enum MessageCode : uint8_t {
+    START   = 0xF0, // Start of transmission
+    END     = 0xF1, // End of transmission
+    ACK     = 0xF2, // Acknowledge
+    NACK    = 0xF3, // Not acknowledged
+    READY   = 0xF4, // Slave has response ready
+    ERROR   = 0xF5, // Error frame
+    RECEIVE = 0xF6, // Asks the receiver to start receiving
+    SEND    = 0xF7, // Asks the receiver to start sending
+    NONE    = 0xF8  // Means nothing to send
+};
+
+
+// Pin definitions
+const int BUZZ_PIN = 2; // External BUZZER pin
+const int SS_PIN = 10;  // Slave Select pin
+
+#define BUFFER_SIZE 128
+char receiving_buffer[BUFFER_SIZE] = {'\0'};
+byte receiving_index = 0;   // No interrupts, so, not volatile
+bool receiving_state = true;
+
+
+void setup() {
+    // Initialize SPI
+    SPI.begin();
+    SPI.setClockDivider(SPI_CLOCK_DIV4);    // Only affects the char transmission
+    SPI.setDataMode(SPI_MODE0);
+    SPI.setBitOrder(MSBFIRST);  // EXPLICITLY SET MSB FIRST! (OTHERWISE is LSB)
+    
+    // Initialize pins
+    pinMode(SS_PIN, OUTPUT);
+    digitalWrite(SS_PIN, HIGH);
+    pinMode(BUZZ_PIN, OUTPUT);
+    digitalWrite(BUZZ_PIN, LOW);
+    
+    // Initialize serial
+    Serial.begin(115200);
+    delay(500);
+    Serial.println("\n\nSPI Master Initialized - Half-Duplex Mode");
+}
+
+void loop() {
+    // Send string commands directly
+    receiveString();    // Before because sendString results in a command that takes time
+    sendString("LED_ON");
+    delay(2000);
+    
+    receiveString();
+    sendString("LED_OFF");
+    delay(2000);
+}
+
+
+#define send_delay_us 10
+
+bool sendString(const char* command) {
+    uint8_t c; // Avoid using 'char' while using values above 127
+    receiving_buffer[0] = '\0'; // Avoids garbage printing
+    receiving_state = false;
+    receiving_index = 0;
+
+    
+    bool successfully_sent = false; // It has to start as false to enter in the next loop
+
+    for (size_t s = 0; !successfully_sent && s < 3; s++) {
+  
+        successfully_sent = true;
+
+        digitalWrite(SS_PIN, LOW);
+        delayMicroseconds(5);
+
+        // Asks the receiver to start receiving
+        SPI.transfer(RECEIVE);
+        delayMicroseconds(send_delay_us);
+        
+        // RECEIVE message code
+        for (uint8_t i = 0; i < BUFFER_SIZE; i++) {
+            if (i > 0) {
+                if (SPI.transfer(command[i]) != command[i - 1])
+                    successfully_sent = false;
+            } else {
+                SPI.transfer(command[0]);
+            }
+            delayMicroseconds(send_delay_us);
+            // Don't make '\0' implicit in order to not have to change the SPDR on the slave side!!
+            if (command[i] == '\0') break;
+        }
+
+        if (SPI.transfer(END) != '\0')  // Because the last char is always '\0'
+            successfully_sent = false;
+
+        delayMicroseconds(5);
+        digitalWrite(SS_PIN, HIGH);
+
+        if (successfully_sent) {
+            Serial.println("Command successfully sent");
+        } else {
+            Serial.print("Command NOT successfully sent on try: ");
+            Serial.println(s + 1);
+            Serial.println("BUZZER activated for 10ms!");
+            digitalWrite(BUZZ_PIN, HIGH);
+            delay(10);  // Buzzer on for 10ms
+            digitalWrite(BUZZ_PIN, LOW);
+        }
+    }
+
+    return successfully_sent;
+}
+
+
+#define receive_delay_us 10 // Receive needs more time to be processed
+
+bool receiveString() {
+    uint8_t c; // Avoid using 'char' while using values above 127
+    receiving_buffer[0] = '\0'; // Avoids garbage printing
+    receiving_state = false;
+    receiving_index = 0;
+
+
+    bool successfully_received = false; // It has to start as false to enter in the next loop
+
+    for (size_t r = 0; !successfully_received && r < 3; r++) {
+  
+        successfully_received = true;
+
+        digitalWrite(SS_PIN, LOW);
+        delayMicroseconds(5);
+
+        // Asks the receiver to start receiving
+        c = SPI.transfer(SEND);
+        delayMicroseconds(send_delay_us);
+            
+        // Starts to receive all chars here
+        for (uint8_t i = 0; i < BUFFER_SIZE; i++) {
+            delayMicroseconds(receive_delay_us);
+            if (i > 0) {    // The first response is discarded
+                c = SPI.transfer(receiving_buffer[i - 1]);
+                if (c == END) {
+                    receiving_buffer[i] = '\0'; // Implicit char
+                    receiving_index = i;
+                    break;
+                } else if (c == ERROR) {
+                    receiving_buffer[0] = '\0'; // Implicit char
+                    receiving_index = 0;
+                    successfully_received = false;
+                    break;
+                } else {
+                    receiving_buffer[i] = c;
+                }
+            } else {
+                c = SPI.transfer('\0');   // Dummy char, not intended to be processed
+                if (c == NONE) {
+                    receiving_buffer[0] = '\0'; // Implicit char
+                    receiving_index = 0;
+                    break;
+                }
+                receiving_buffer[0] = c;   // Dummy char, not intended to be processed
+            }
+        }
+
+        delayMicroseconds(5);
+        digitalWrite(SS_PIN, HIGH);
+
+        if (successfully_received) {
+            if (receiving_index > 0) {
+                Serial.print("Received message: ");
+                Serial.println(receiving_buffer);
+            } else {
+                Serial.println("Nothing received");
+            }
+        } else {
+            Serial.print("Message NOT successfully received on try: ");
+            Serial.println(r + 1);
+            Serial.println("BUZZER activated for 2 x 10ms!");
+            digitalWrite(BUZZ_PIN, HIGH);
+            delay(10);  // Buzzer on for 10ms
+            digitalWrite(BUZZ_PIN, LOW);
+            delay(100);
+            digitalWrite(BUZZ_PIN, HIGH);
+            delay(10);  // Buzzer on for 10ms
+            digitalWrite(BUZZ_PIN, LOW);
+        }
+    }
+
+    return successfully_received;
+}
+
+
---
+++ SPI_Wire/NanoNano_JSON/NanoSlave/NanoSlave.ino
@@ -0,0 +1,152 @@
+// SPI Slave Code - Pure String Commands
+#include <SPI.h>
+
+
+enum MessageCode : uint8_t {
+    START   = 0xF0, // Start of transmission
+    END     = 0xF1, // End of transmission
+    ACK     = 0xF2, // Acknowledge
+    NACK    = 0xF3, // Not acknowledged
+    READY   = 0xF4, // Slave has response ready
+    ERROR   = 0xF5, // Error frame
+    RECEIVE = 0xF6, // Asks the receiver to start receiving
+    SEND    = 0xF7, // Asks the receiver to start sending
+    NONE    = 0xF8  // Means nothing to send
+};
+
+
+// Pin definitions
+const int GREEN_LED_PIN = 2;
+const int YELLOW_LED_PIN = 21;  // A7 = digital pin 21
+const int SS_PIN = 10;
+
+#define BUFFER_SIZE 128
+char receiving_buffer[BUFFER_SIZE] = {'\0'};
+char sending_buffer[BUFFER_SIZE] = {'\0'};
+
+volatile byte receiving_index = 0;
+volatile byte sending_index = 0;
+
+volatile bool receiving_state = false;
+volatile bool sending_state = false;
+volatile bool process_message = false;
+
+
+void setup() {
+    Serial.begin(115200);
+    delay(500);
+    Serial.println("\n\nSPI Slave Ready - Half-Duplex Mode");
+
+    Serial.print("LED_BUILTIN: ");  // Led 13 is already used by SCK
+    Serial.println(LED_BUILTIN);
+
+    pinMode(GREEN_LED_PIN, OUTPUT);
+    digitalWrite(GREEN_LED_PIN, LOW);
+    pinMode(YELLOW_LED_PIN, OUTPUT);
+    digitalWrite(YELLOW_LED_PIN, LOW);
+
+    pinMode(MISO, OUTPUT);  // MISO must be OUTPUT for Slave to send data!
+
+    // Initialize SPI as slave - EXPLICIT MSB FIRST
+    SPCR = 0;  // Clear register
+    SPCR |= _BV(SPE);    // SPI Enable
+    SPCR |= _BV(SPIE);   // SPI Interrupt Enable  
+    SPCR &= ~_BV(DORD);  // MSB First (DORD=0 for MSB first)
+    SPCR &= ~_BV(CPOL);  // Clock polarity 0
+    SPCR &= ~_BV(CPHA);  // Clock phase 0 (MODE0)
+
+    // // Initialize SPI as slave
+    // SPCR |= _BV(SPE);
+    // SPCR |= _BV(SPIE);
+
+    SPI.attachInterrupt();  
+}
+
+
+
+// ------------------------------
+// SPI Interrupt
+// ------------------------------
+ISR(SPI_STC_vect) {
+
+    // AVOID PLACING HEAVY CODE OR CALL HERE. THIS INTERRUPTS THE LOOP!
+
+    // WARNING:
+    //     AVOID PLACING Serial.print CALLS HERE BECAUSE IT WILL DELAY 
+    //     THE POSSIBILITY OF SPI CAPTURE AND RESPONSE IN TIME !!!
+
+    uint8_t c = SPDR;    // Avoid using 'char' while using values above 127
+
+    if (c == RECEIVE) {
+        receiving_state = true;
+        receiving_index = 0;
+    } else if (c == SEND) {
+        sending_index = 0;
+        if (sending_buffer[sending_index] == '\0') {
+            SPDR = NONE;    // Nothing to send
+        } else {
+            SPDR = sending_buffer[sending_index++];
+            sending_state = true;
+        }
+    } else if (sending_state) {
+        if (sending_index > 1 && c != sending_buffer[sending_index - 2]) {  // Two messages delay
+            sending_state = false;
+            SPDR = ERROR;
+        } else if (sending_buffer[sending_index] == '\0') {
+            sending_state = false;
+            sending_buffer[0] = '\0';   // Makes sure the sending buffer is marked as empty
+            SPDR = END;     // Nothing more to send (spares extra send, '\0' implicit)
+        } else if (sending_index < BUFFER_SIZE) {
+            SPDR = sending_buffer[sending_index++];
+        } else {
+            sending_state = false;
+            SPDR = ERROR;
+        }
+    } else if (c == END) {
+        receiving_state = false;
+        process_message = true;
+    } else if (receiving_state) {
+        if (receiving_index < BUFFER_SIZE) {
+            receiving_buffer[receiving_index++] = c;
+        } else {
+            receiving_state = false;
+            SPDR = ERROR;
+        }
+    }
+}
+
+
+void processMessage() {
+
+    Serial.print("Processed command: ");
+    Serial.println(receiving_buffer);
+
+
+    if (strcmp(receiving_buffer, "LED_ON") == 0) {
+        digitalWrite(GREEN_LED_PIN, HIGH);
+        strcpy(sending_buffer, "OK_ON");
+        Serial.print("LED is ON");
+        Serial.print(" | Sending: ");
+        Serial.println(sending_buffer);
+    } else if (strcmp(receiving_buffer, "LED_OFF") == 0) {
+        digitalWrite(GREEN_LED_PIN, LOW);
+        strcpy(sending_buffer, "OK_OFF");
+        Serial.print("LED is OFF");
+        Serial.print(" | Sending: ");
+        Serial.println(sending_buffer);
+    } else {
+        strcpy(sending_buffer, "BUZZ");
+        Serial.print("Unknown command");
+        Serial.print(" | Sending: ");
+        Serial.println(sending_buffer);
+    }
+}
+
+void loop() {
+    // HEAVY PROCESSING SHALL BE IN THE LOOP
+    if (process_message) {
+        processMessage();   // Called only once!
+        process_message = false;    // Critical to avoid repeated calls over the ISR function
+    }
+}
+
