dc11ee1cc1e6fd65d9ac0a4c5bf500d745fda1df
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-03 07:42:35 +0000

    Prints out the LED 21

--- SPI_Wire/NanoNano_JSON/NanoSlave_class/NanoSlave_class.ino
+++ SPI_Wire/NanoNano_JSON/NanoSlave_class/NanoSlave_class.ino
@@ -9,18 +9,22 @@ ISR(SPI_STC_vect) {
 
 
 // Create instance
-Slave_class slave_class;
+Slave_class slave_class;    // Sets the LEDS
 
 
 void setup() {
     // Initialize serial
     Serial.begin(115200);
+    digitalWrite(YELLOW_LED_PIN, HIGH);
     delay(500);
+    digitalWrite(YELLOW_LED_PIN, LOW);
 
     // Enable global interrupts
     sei();
 
     Serial.println("\n\nSPI Slave Initialized - JSON class Mode");
+    Serial.print("YELLOW_LED_PIN: ");  // Led 13 is already used by SCK
+    Serial.println(YELLOW_LED_PIN);
 }
 
 
