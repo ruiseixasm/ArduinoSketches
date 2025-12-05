5d4eda9665775db8591b4739305347c88c271a63
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-05 15:04:21 +0000

    Working drivers

--- PlatformIO/BlinkBlue/src/main.cpp
+++ PlatformIO/BlinkBlue/src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+// Most ESP32 dev boards have LED on GPIO 2
+#define LED_PIN 2
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // put your setup code here, to run once:
+    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(115200);
+    Serial.println("ESP32 Blink Started!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("LED ON");
+    delay(1000);
+
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("LED OFF");
+    delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
