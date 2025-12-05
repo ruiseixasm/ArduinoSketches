f96219b13ca13cffb531ea0f4cc7a31a267ddc14
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-05 16:54:44 +0000

    Added documentation in how to compile and upload

---
+++ PlatformIO/HOW_TO_COMPILE_AND_UPLOAD.md
@@ -0,0 +1,6 @@
+# RUN
+pio run --project-dir .
+# UPLOAD
+pio run -t upload --project-dir .
+# MONITOR
+pio device monitor --project-dir .
--- SPI_ESP32/ESP32_JSON/ESP32_Master_class_static_members_PIO/src/main.cpp
+++ SPI_ESP32/ESP32_JSON/ESP32_Master_class_static_members_PIO/src/main.cpp
@@ -2,6 +2,8 @@
 // 		pio run --project-dir .
 // UPLOAD:
 // 		pio run -t upload --project-dir .
+// MONITOR:
+//		pio device monitor --project-dir .
 
 
 // SPI Master Code - Pure String Commands
