350836e264ac67454e9f7518ee1bb77154c8c9f0
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-09 13:53:12 +0000

    Updated library

--- JsonMidiCreator/TalkieEthernetENC/src/JsonTalker.cpp
+++ JsonMidiCreator/TalkieEthernetENC/src/JsonTalker.cpp
@@ -23,9 +23,9 @@ bool JsonTalker::_is_led_on = false;
 
 
 bool JsonTalker::remoteSend(JsonObject json_message, bool as_reply) {
-    if (_socket == nullptr) return false;
+    if (_muted || _socket == nullptr) return false;
     json_message["f"] = _name;
-    json_message["c"] = 0;  // 'c' = 0 means REMOTE communication
+    // 'c' = 0 means REMOTE communication (already set by socket's remoteSend)
     return _socket->remoteSend(json_message, as_reply);
 }
 
@@ -38,7 +38,7 @@ uint8_t JsonTalker::get_delay() {
     return _socket->get_max_delay();
 }
 
-long JsonTalker::get_total_drops() {
+uint16_t JsonTalker::get_total_drops() {
     return _socket->get_drops_count();
 }
 
