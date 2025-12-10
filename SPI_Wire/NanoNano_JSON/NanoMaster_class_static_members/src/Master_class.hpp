7012d0113688d1ead7e4ad22e454b46f37b62263
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-10 02:27:44 +0000

    Added debugging prints

--- SPI_Wire/NanoNano_JSON/NanoMaster_class_static_members/src/Master_class.hpp
+++ SPI_Wire/NanoNano_JSON/NanoMaster_class_static_members/src/Master_class.hpp
@@ -70,9 +70,24 @@ private:
                 delayMicroseconds(send_delay_us);
                 if (i > 0) {
                     if (command[i - 1] == '\0') {
-                        c = SPI.transfer(END);
+                        c = SPI.transfer(END);	// Checks the ACK response bellow
                         if (c == '\0') {
-                            length = i;
+                			delayMicroseconds(send_delay_us);
+                        	c = SPI.transfer(END);	// ACK response
+                        	#ifdef MASTER_CLASS_DEBUG
+							Serial.println("\t\tSent END");
+                        	#endif
+							if (c == ACK) {
+                        		#ifdef MASTER_CLASS_DEBUG
+								Serial.println("\t\tReceived ACK");
+                        		#endif
+                            	length = i;
+							} else {
+                        		#ifdef MASTER_CLASS_DEBUG
+								Serial.println("\t\tDid NOT received ACK");
+                        		#endif
+								length = 0;	// Try again
+							}
                             break;
                         } else {
                             length = 0;
