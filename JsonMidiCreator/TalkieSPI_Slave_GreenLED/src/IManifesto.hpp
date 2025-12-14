ac042a5cd8bc625bd56f0788e97023f5fdd717cc
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-14 03:56:09 +0000

    Added count methods to the IManifesto

---
+++ JsonMidiCreator/TalkieSPI_Slave_GreenLED/src/IManifesto.hpp
@@ -0,0 +1,69 @@
+/*
+JsonTalkie - Json Talkie is intended for direct IoT communication.
+Original Copyright (c) 2025 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonTalkie
+*/
+#ifndef I_MANIFESTO_HPP
+#define I_MANIFESTO_HPP
+
+#include <Arduino.h>
+
+
+class IManifesto {
+public:
+
+    IManifesto(const IManifesto&) = delete;
+    IManifesto& operator=(const IManifesto&) = delete;
+    IManifesto(IManifesto&&) = delete;
+    IManifesto& operator=(IManifesto&&) = delete;
+    
+    IManifesto() = default;
+    virtual ~IManifesto() = default;
+
+    struct Action {
+        const char* name;
+        const char* desc;
+    };
+
+    virtual const char* class_name() const { return "IManifesto"; }
+
+    
+    // Size methods
+    virtual uint8_t runsCount() const = 0;
+    virtual uint8_t setsCount() const = 0;
+    virtual uint8_t getsCount() const = 0;
+
+
+    // These methods are intended to be used in a for loop where they return an Action at each iteration
+    virtual bool iterateRunsReset() = 0;
+    virtual Action* iterateRunsNext() = 0;  // Returns nullptr when done
+
+    virtual bool iterateSetsReset() = 0;
+    virtual Action* iterateSetsNext() = 0;
+    
+    virtual bool iterateGetsReset() = 0;
+    virtual Action* iterateGetsNext() = 0;
+
+
+    // These methods are intended to call the respective ByIndex when it's find based on a name
+    virtual bool runByName(const char* name) = 0;
+    virtual bool setByName(const char* name, const uint32_t value) = 0;
+    virtual uint32_t getByName(const char* name) = 0;
+
+
+    // These methods use a switch that based on the index pick the respective action to be done
+    virtual bool runByIndex(const uint8_t index) = 0;
+    virtual bool setByIndex(const uint8_t index, const uint32_t value) = 0;
+    virtual uint32_t getByIndex(const uint8_t index) = 0;
+};
+
+
+#endif // I_MANIFESTO_HPP
