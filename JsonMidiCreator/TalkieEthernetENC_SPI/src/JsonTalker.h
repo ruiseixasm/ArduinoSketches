27fee478596396f67b6b243896a4a8e8943f6932
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-17 17:06:43 +0000

    Code made safer

--- JsonMidiCreator/TalkieEthernetENC_SPI/src/JsonTalker.h
+++ JsonMidiCreator/TalkieEthernetENC_SPI/src/JsonTalker.h
@@ -49,7 +49,7 @@ protected:
     const char* _name;      // Name of the Talker
     const char* _desc;      // Description of the Device
 	IManifesto* _manifesto = nullptr;
-    uint8_t _channel = 0;
+    uint8_t _channel = static_cast<uint8_t>(millis());	// Avoids a common channel for everyone (OTHERWISE DANGEROUS)
     bool _muted_action = false;
 
 public:
