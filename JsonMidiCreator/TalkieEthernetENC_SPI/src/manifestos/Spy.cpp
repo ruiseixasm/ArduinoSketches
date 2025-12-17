c915b40ec8e9b8a789e05414fbc5a21b24022e5b
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-17 19:01:04 +0000

    Clean code

--- JsonMidiCreator/TalkieEthernetENC_SPI/src/manifestos/Spy.cpp
+++ JsonMidiCreator/TalkieEthernetENC_SPI/src/manifestos/Spy.cpp
@@ -33,7 +33,7 @@ bool Spy::runByIndex(uint8_t index, JsonObject& json_message, JsonTalker* talker
 					// from-to already reversed
 					// In the beginning I know no one, so I ask them to talk
 					json_message[ JsonKey::MESSAGE ] = static_cast<int>(MessageData::TALK);
-					// Keep track of the original talker
+					// Keep track of the original talker (Already swapped here, that's why TO is used)
 					original_talker = json_message[ JsonKey::TO ].as<String>();	// Explicit conversion
 					json_message.remove( JsonKey::TO );	// Needs to be for everybody
 					talker->localSend(json_message);	// Only inquires locally
@@ -54,36 +54,37 @@ void Spy::echo(JsonObject& json_message, JsonTalker* talker) {
 	if (source_data == SourceData::LOCAL) {
 
 		MessageData original_message = static_cast<MessageData>( json_message[ JsonKey::ORIGINAL ].as<int>() );
-
 		switch (original_message) {
 
-		case MessageData::TALK:
-			// from-to not yet reversed, doing it now
-			json_message[ JsonKey::TO ] = json_message[ JsonKey::FROM ];
-			json_message[ JsonKey::FROM ] = talker->get_name();
-			// Now that they started to talk, I ask for more information
-			json_message[ JsonKey::MESSAGE ] = static_cast<int>(MessageData::SYS);
-			json_message[ JsonKey::SYSTEM ] = static_cast<int>(SystemData::PING);
-			// The id will be set by me at the exit point (same clock)
-			talker->localSend(json_message);	// Only inquires locally
-			break;
-		
-		case MessageData::SYS:
-			// Need to check first if it's a answer to a ping
-			if (json_message[ JsonKey::SYSTEM ] == static_cast<int>(SystemData::PING)) {
-				// Time to calculate the delay
-				uint16_t actual_time = static_cast<uint16_t>(millis());
-				uint16_t message_time = json_message[ JsonKey::TIMESTAMP ];
-				uint16_t time_delay = actual_time - message_time;
-				// Report back to the original talker T as echo (it's waiting for it)
-				json_message[ JsonKey::TO ] = original_talker;	// Already an echo message
-				json_message[ JsonKey::VALUE ] = time_delay;
-				talker->remoteSend(json_message);	// Finally answers to the REMOTE caller
-			}
-			break;
+			case MessageData::TALK:
+				// from-to not yet reversed, doing it now
+				json_message[ JsonKey::TO ] = json_message[ JsonKey::FROM ];
+				json_message[ JsonKey::FROM ] = talker->get_name();
+				// Now that they started to talk, I ask for more information
+				json_message[ JsonKey::MESSAGE ] = static_cast<int>(MessageData::SYS);
+				json_message[ JsonKey::SYSTEM ] = static_cast<int>(SystemData::PING);
+				// Removed the DESCRIPTION to avoid extra load
+				json_message.remove( JsonKey::DESCRIPTION );
+				// The id will be set by me at the exit point (same clock)
+				talker->localSend(json_message);	// Only inquires locally
+				break;
+			
+			case MessageData::SYS:
+				if (json_message[ JsonKey::TIMESTAMP ].is<uint16_t>()) {
+					// Need to check first if it's a answer to a ping
+					if (json_message[ JsonKey::SYSTEM ] == static_cast<int>(SystemData::PING)) {
+						// Time to calculate the delay
+						uint16_t actual_time = static_cast<uint16_t>(millis());
+						uint16_t message_time = json_message[ JsonKey::TIMESTAMP ].as<uint16_t>();
+						uint16_t time_delay = actual_time - message_time;
+						// Report back to the original talker T as echo (it's waiting for it)
+						json_message[ JsonKey::TO ] = original_talker;	// Already an echo message
+						json_message[ JsonKey::VALUE ] = time_delay;
+						talker->remoteSend(json_message);	// Finally answers to the REMOTE caller
+					}
+				}
+				break;
 
-		default:
-			break;
 		}
 	}
 }
