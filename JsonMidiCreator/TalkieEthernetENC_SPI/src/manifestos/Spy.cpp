7ac25b161e25d8dfbe856a8ce3d4901bf5274b9c
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-18 12:19:23 +0000

    Returns Reply as FROM

--- JsonMidiCreator/TalkieEthernetENC_SPI/src/manifestos/Spy.cpp
+++ JsonMidiCreator/TalkieEthernetENC_SPI/src/manifestos/Spy.cpp
@@ -81,6 +81,7 @@ void Spy::echo(JsonObject& json_message, JsonTalker* talker) {
 					uint16_t message_time = json_message[ JsonKey::TIMESTAMP ].as<uint16_t>();	// must have
 					uint16_t time_delay = actual_time - message_time;
 					json_message[ JsonKey::VALUE ] = time_delay;
+					json_message[ JsonKey::REPLY ] = json_message[ JsonKey::FROM ];	// Informs the Talker as reply
 					// Prepares headers for the original REMOTE sender
 					json_message[ JsonKey::TO ] = _original_talker;
 					json_message[ JsonKey::FROM ] = talker->get_name();	// Avoids swapping
