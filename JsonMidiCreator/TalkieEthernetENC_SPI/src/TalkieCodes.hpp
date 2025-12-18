01180b9c3589ba1e1125f7fc4fa60df502cb61cd
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-18 17:56:08 +0000

    Added method valueData

--- JsonMidiCreator/TalkieEthernetENC_SPI/src/TalkieCodes.hpp
+++ JsonMidiCreator/TalkieEthernetENC_SPI/src/TalkieCodes.hpp
@@ -41,6 +41,22 @@ struct TalkieCodes {
     };
 
 
+	static const char* valueData(size_t nth = 0) {
+		switch (nth) {
+			case 1: 	return "1";
+			case 2: 	return "2";
+			case 3: 	return "3";
+			case 4: 	return "4";
+			case 5: 	return "5";
+			case 6: 	return "6";
+			case 7: 	return "7";
+			case 8: 	return "8";
+			case 9: 	return "9";
+			default: 	return "0";
+		}
+	}
+
+
 	enum class SourceData : int {
 		REMOTE, LOCAL, HERE
 	};
