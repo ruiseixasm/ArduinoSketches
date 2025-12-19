59b4f304a7ebcb0f4846e79b25adc4a6c070f24b
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-19 11:11:52 +0000

    Created class BlueManifesto

---
+++ JsonMidiCreator/TalkieEthernetENC_SPI/src/manifestos/BlueManifesto.hpp
@@ -0,0 +1,131 @@
+/*
+JsonTalkie - Json Talkie is intended for direct IoT communication.
+Original Copyright (c) 2025 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonTalkie
+*/
+#ifndef GREEN_MANIFESTO_HPP
+#define GREEN_MANIFESTO_HPP
+
+#include "../TalkerManifesto.hpp"
+
+// #define GREEN_TALKER_DEBUG
+
+
+class BlueManifesto : public TalkerManifesto {
+public:
+
+    const char* class_name() const override { return "BlueManifesto"; }
+
+    BlueManifesto(uint8_t led_pin) : TalkerManifesto(), _led_pin(led_pin)
+	{
+		pinMode(_led_pin, OUTPUT);
+	}	// Constructor
+
+    ~BlueManifesto()
+	{
+		digitalWrite(_led_pin, LOW);
+		pinMode(_led_pin, INPUT);
+	}	// Destructor
+
+
+protected:
+
+	const uint8_t _led_pin;
+    bool _is_led_on = false;  	// keep track of state yourself, by default it's off
+    uint16_t _total_calls = 0;
+
+
+    Action calls[3] = {
+		{"on", "Turns led ON"},
+		{"off", "Turns led OFF"},
+		{"actions", "Returns the number of triggered Actions"}
+    };
+    
+    const Action* getActionsArray() const override { return calls; }
+
+    // Size methods
+    uint8_t actionsCount() const override { return sizeof(calls)/sizeof(Action); }
+
+
+public:
+    
+    // Index-based operations (simplified examples)
+    bool actionByIndex(uint8_t index, JsonObject& json_message, JsonTalker* talker) override {
+        (void)talker;		// Silence unused parameter warning
+		
+		if (index >= sizeof(calls)/sizeof(Action)) return false;
+		
+		// Actual implementation would do something based on index
+		switch(index) {
+
+			case 0:
+			{
+				#ifdef GREEN_MANIFESTO_DEBUG
+				Serial.println(F("\tCase 0 - Turning LED ON"));
+				#endif
+		
+				if (!_is_led_on) {
+				#ifdef GREEN_LED
+					#ifdef GREEN_MANIFESTO_DEBUG
+						Serial.print(F("\tGREEN_LED IS DEFINED as: "));
+						Serial.println(GREEN_LED);
+					#endif
+					digitalWrite(GREEN_LED, HIGH);
+				#else
+					#ifdef GREEN_MANIFESTO_DEBUG
+						Serial.println(F("\tGREEN_LED IS NOT DEFINED in this context!"));
+					#endif
+				#endif
+					_is_led_on = true;
+					_total_calls++;
+					return true;
+				} else {
+					json_message[ valueKey(0) ] = "Already On!";
+					return false;
+				}
+			}
+			break;
+
+			case 1:
+			{
+				#ifdef GREEN_MANIFESTO_DEBUG
+				Serial.println(F("\tCase 1 - Turning LED OFF"));
+				#endif
+		
+				if (_is_led_on) {
+				#ifdef GREEN_LED
+					digitalWrite(GREEN_LED, LOW);
+				#endif
+					_is_led_on = false;
+					_total_calls++;
+				} else {
+					json_message[ valueKey(0) ] = "Already Off!";
+					return false;
+				}
+				return true;
+			}
+			break;
+			
+            case 2:
+                _bpm_10 = json_message[ valueKey(0) ].as<uint16_t>();
+                return true;
+                break;
+				
+            // case 3:
+			// 	return _bpm_10;
+		}
+		return false;
+	}
+    
+};
+
+
+#endif // GREEN_MANIFESTO_HPP
