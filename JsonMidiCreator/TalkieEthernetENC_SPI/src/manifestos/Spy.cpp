4d288657884d5b17e7616b9d8e9388f4e025c03b
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-19 20:43:24 +0000

    Improved

--- JsonMidiCreator/TalkieEthernetENC_SPI/src/manifestos/Spy.cpp
+++ JsonMidiCreator/TalkieEthernetENC_SPI/src/manifestos/Spy.cpp
@@ -41,6 +41,7 @@ bool Spy::actionByIndex(uint8_t index, JsonObject& json_message, JsonTalker* tal
 					_original_message.message_data = MessageValue::PING;	// It's is the emulated message (not CALL)
 					// 2. Repurpose it to be a LOCAL PING
 					json_message[ TalkieKey::MESSAGE ] = static_cast<int>(MessageValue::PING);
+					remove(json_message[ TalkieKey::TIMESTAMP ]);	// Makes sure a new TIMESTAMP is set (same as IDENTITY)
 					if (json_message[ dataKey(0) ].is<String>()) {
 						json_message[ TalkieKey::TO ] = json_message[ dataKey(0) ].as<String>();
 					} else {	// Removes the original TO
@@ -61,6 +62,7 @@ bool Spy::actionByIndex(uint8_t index, JsonObject& json_message, JsonTalker* tal
 					_original_message.message_data = MessageValue::PING;	// It's is the emulated message (not CALL)
 					// 2. Repurpose it to be a SELF PING
 					json_message[ TalkieKey::MESSAGE ] = static_cast<int>(MessageValue::PING);
+					remove(json_message[ TalkieKey::TIMESTAMP ]);	// Makes sure a new TIMESTAMP is set (same as IDENTITY)
 					// 3. Sends the message to myself
 					talker->selfSend(json_message);	// Dispatches it directly as LOCAL
 					// 4. Finally, makes sure the message isn't returned to the REMOTE sender by setting its source as NONE
