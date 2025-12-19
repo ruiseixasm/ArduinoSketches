5a0359f3151bddf2921affdba5d452d066a61171
Rui Monteiro <ruiseixas@yahoo.com>
2025-12-19 20:51:47 +0000

    Improved code

--- JsonMidiCreator/TalkieEthernetENC_SPI/src/JsonTalker.h
+++ JsonMidiCreator/TalkieEthernetENC_SPI/src/JsonTalker.h
@@ -253,7 +253,7 @@ public:
 
 		// Tags the message as LOCAL sourced
 		json_message[ TalkieKey::SOURCE ] = static_cast<int>(SourceValue::SELF);
-		// Despite being an SELF message it also needs to be prepared like any other
+		// Despite being a SELF message it also needs to be prepared like any other
 		if (prepareMessage(json_message)) {
 			return processMessage(json_message);
 		}
@@ -264,7 +264,11 @@ public:
 	virtual bool noneSend(JsonObject& json_message) {
 		// Tags the message as NONE sourced
 		json_message[ TalkieKey::SOURCE ] = static_cast<int>(SourceValue::NONE);
-		return true;	// None accepts everything and does nothing
+		// Despite being a NONE message it also needs to be prepared like any other
+		if (prepareMessage(json_message)) {
+			return processMessage(json_message);
+		}
+		return false;
 	}
 
 
