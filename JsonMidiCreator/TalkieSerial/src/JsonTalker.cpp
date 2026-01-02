f63cfe5bceced69c4d9356e3cde74f5f4b19a3cc
Rui Monteiro <ruiseixas@yahoo.com>
2026-01-02 22:22:50 +0000

    Added the Serial sketch

---
+++ JsonMidiCreator/TalkieSerial/src/JsonTalker.cpp
@@ -0,0 +1,102 @@
+/*
+JsonTalkie - Json Talkie is intended for direct IoT communication.
+Original Copyright (c) 2025 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonTalkie
+*/
+
+#include "JsonTalker.h"
+#include "MessageRepeater.hpp"
+
+
+void JsonTalker::setLink(MessageRepeater* message_repeater, LinkType link_type) {
+	_message_repeater = message_repeater;
+	_link_type = link_type;
+}
+
+
+bool JsonTalker::transmitToRepeater(JsonMessage& json_message) {
+
+	#ifdef JSON_TALKER_DEBUG_NEW
+	Serial.print(F("\t\t\ttransmitToRepeater(Talker): "));
+	json_message.write_to(Serial);
+	Serial.println();  // optional: just to add a newline after the JSON
+	#endif
+
+	if (_message_repeater && prepareMessage(json_message)) {
+		switch (_link_type) {
+			case LinkType::TALKIE_LT_UP_LINKED:
+			case LinkType::TALKIE_LT_UP_BRIDGED:
+				return _message_repeater->talkerDownlink(*this, json_message);
+			case LinkType::TALKIE_LT_DOWN_LINKED:
+				return _message_repeater->talkerUplink(*this, json_message);
+			default: return false;
+		}
+	}
+}
+
+
+bool JsonTalker::transmissionSockets(JsonMessage& json_message) {
+	if (_message_repeater) {
+		uint8_t socket_index = 0;
+		const BroadcastSocket* socket;
+		_message_repeater->iterateSocketsReset();
+		while ((socket = _message_repeater->iterateSocketNext()) != nullptr) {	// No boilerplate
+			json_message.set_nth_value_number(0, socket_index++);
+			json_message.set_nth_value_string(1, socket->class_name());
+			transmitToRepeater(json_message);	// Many-to-One
+		}
+		return socket_index > 0;
+	}
+	return false;
+}
+
+
+bool JsonTalker::transmissionDrops(JsonMessage& json_message) {
+	if (_message_repeater) {
+		uint8_t socket_index = 0;
+		const BroadcastSocket* socket;
+		_message_repeater->iterateSocketsReset();
+		while ((socket = _message_repeater->iterateSocketNext()) != nullptr) {	// No boilerplate
+			json_message.set_nth_value_number(0, socket_index++);
+			json_message.set_nth_value_number(1, socket->get_drops_count());
+			transmitToRepeater(json_message);	// Many-to-One
+		}
+		return socket_index > 0;
+	}
+	return false;
+}
+
+
+bool JsonTalker::transmissionDelays(JsonMessage& json_message) {
+	if (_message_repeater) {
+		uint8_t socket_index = 0;
+		const BroadcastSocket* socket;
+		_message_repeater->iterateSocketsReset();
+		while ((socket = _message_repeater->iterateSocketNext()) != nullptr) {	// No boilerplate
+			json_message.set_nth_value_number(0, socket_index++);
+			json_message.set_nth_value_number(1, socket->get_max_delay());
+			transmitToRepeater(json_message);	// Many-to-One
+		}
+		return socket_index > 0;
+	}
+	return false;
+}
+
+
+bool JsonTalker::setSocketDelay(uint8_t socket_index, uint8_t delay_value) const {
+	if (_message_repeater) {
+		BroadcastSocket* socket = _message_repeater->accessSocket(socket_index);
+		socket->set_max_delay(delay_value);
+		return true;
+	}
+	return false;
+}
+
---
+++ JsonMidiCreator/TalkieSerial/src/TalkieCodes.hpp
@@ -0,0 +1,104 @@
+/*
+JsonTalkie - Json Talkie is intended for direct IoT communication.
+Original Copyright (c) 2025 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonTalkie
+*/
+#ifndef TALKIE_CODES_HPP
+#define TALKIE_CODES_HPP
+
+#include <Arduino.h>        // Needed for Serial given that Arduino IDE only includes Serial in .ino files!
+
+
+// PREFIXED VERSION - 100% safe across all boards
+struct TalkieCodes {
+
+	enum ValueType : uint8_t {
+		TALKIE_VT_STRING,
+		TALKIE_VT_INTEGER,
+		TALKIE_VT_OTHER,
+		TALKIE_VT_VOID
+	};
+
+	
+	enum LinkType : uint8_t {
+		TALKIE_LT_NONE,
+		TALKIE_LT_DOWN_LINKED,
+		TALKIE_LT_UP_LINKED,
+		TALKIE_LT_UP_BRIDGED	// TALKIE_LT_UP_BRIDGED allows the sending of LOCAL broadcasted messages
+	};
+
+
+	enum TalkerMatch : uint8_t {
+		TALKIE_MATCH_NONE,
+		TALKIE_MATCH_ANY,
+		TALKIE_MATCH_BY_CHANNEL,
+		TALKIE_MATCH_BY_NAME,
+		TALKIE_MATCH_FAIL
+	};
+
+
+	enum BroadcastValue : uint8_t {
+		TALKIE_BC_REMOTE,
+		TALKIE_BC_LOCAL,
+		TALKIE_BC_SELF,
+		TALKIE_BC_NONE
+	};
+
+
+    enum MessageValue : uint8_t {
+        TALKIE_MSG_TALK,
+        TALKIE_MSG_CHANNEL,
+        TALKIE_MSG_PING,
+        TALKIE_MSG_CALL,
+        TALKIE_MSG_LIST,
+        TALKIE_MSG_SYSTEM,
+        TALKIE_MSG_ECHO,
+        TALKIE_MSG_ERROR,
+        TALKIE_MSG_NOISE
+    };
+
+
+    enum SystemValue : uint8_t {
+        TALKIE_SYS_BOARD,
+        TALKIE_SYS_MUTE,
+        TALKIE_SYS_DROPS,
+        TALKIE_SYS_DELAY,
+        TALKIE_SYS_SOCKET,
+        TALKIE_SYS_MANIFESTO,
+        TALKIE_SYS_UNDEFINED
+    };
+
+
+    enum RogerValue : uint8_t {
+        TALKIE_RGR_ROGER,
+		TALKIE_RGR_NEGATIVE,
+		TALKIE_RGR_SAY_AGAIN,
+		TALKIE_RGR_NIL,
+		TALKIE_RGR_NO_JOY
+    };
+
+
+    enum ErrorValue : uint8_t {
+        TALKIE_ERR_CHECKSUM,
+        TALKIE_ERR_MESSAGE,
+        TALKIE_ERR_IDENTITY,
+        TALKIE_ERR_FIELD,
+        TALKIE_ERR_FROM,
+        TALKIE_ERR_TO,
+        TALKIE_ERR_DELAY,
+        TALKIE_ERR_KEY,
+        TALKIE_ERR_VALUE,
+        TALKIE_ERR_UNDEFINED
+    };
+};
+
+
+#endif // TALKIE_CODES_HPP
