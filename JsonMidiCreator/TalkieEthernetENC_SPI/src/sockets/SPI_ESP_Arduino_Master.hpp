8a728ecb73f53639e926205f047679a7264e948f
Rui Monteiro <ruiseixas@yahoo.com>
2026-01-04 02:15:06 +0000

    Renamed Macro variables

--- JsonMidiCreator/TalkieEthernetENC_SPI/src/sockets/SPI_ESP_Arduino_Master.hpp
+++ JsonMidiCreator/TalkieEthernetENC_SPI/src/sockets/SPI_ESP_Arduino_Master.hpp
@@ -11,8 +11,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 Lesser General Public License for more details.
 https://github.com/ruiseixasm/JsonTalkie
 */
-#ifndef SPI_ESP_ARDUINO_MASTER_VSPI_HPP
-#define SPI_ESP_ARDUINO_MASTER_VSPI_HPP
+#ifndef SPI_ESP_ARDUINO_MASTER_HPP
+#define SPI_ESP_ARDUINO_MASTER_HPP
 
 
 #include <SPI.h>
@@ -713,4 +713,4 @@ public:
 
 
 
-#endif // SPI_ESP_ARDUINO_MASTER_VSPI_HPP
+#endif // SPI_ESP_ARDUINO_MASTER_HPP
