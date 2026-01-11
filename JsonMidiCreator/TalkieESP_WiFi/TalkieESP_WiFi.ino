6f57b0c475ea8e026179e619ef9808a457a91afe
Rui Monteiro <ruiseixas@yahoo.com>
2026-01-11 19:04:52 +0000

    Added Broadcast value

--- JsonMidiCreator/TalkieESP_WiFi/TalkieESP_WiFi.ino
+++ JsonMidiCreator/TalkieESP_WiFi/TalkieESP_WiFi.ino
@@ -130,9 +130,10 @@ void setup() {
 	// ESP8266 has the HIGH and LOW inverted, so, the LED has to be set as HIG to be turned off
 	#if defined(ESP8266)
 
-	Serial.println("\tSending 'turn_off' message");
 	JsonMessage turn_off;
 	turn_off.set_message_value(MessageValue::TALKIE_MSG_CALL);
+	turn_off.set_broadcast_value(BroadcastValue::TALKIE_BC_LOCAL);
+	Serial.println("\tSending 'turn_off' message");
 	turn_off.set_to_name("blue");	// The blue Talker is the one that controls the blue light
 	turn_off.set_action_name("on");	// Because on is off ion the ESP8266
 	message_repeater.downlinkMessage(turn_off);
