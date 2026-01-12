32a4801a7c08937ba2929093094ca3d64e822218
Rui Monteiro <ruiseixas@yahoo.com>
2026-01-12 00:56:49 +0000

    Corrected naming

--- JsonMidiCreator/TalkieESP_WiFi/TalkieESP_WiFi.ino
+++ JsonMidiCreator/TalkieESP_WiFi/TalkieESP_WiFi.ino
@@ -130,7 +130,7 @@ void setup() {
 	JsonMessage turn_off;
 	turn_off.set_message_value(MessageValue::TALKIE_MSG_CALL);
 	turn_off.set_broadcast_value(BroadcastValue::TALKIE_BC_LOCAL);
-	turn_off.set_to_name("blue");	// The blue Talker is the one that controls the blue light
+	turn_off.set_to_name("blue2");	// The blue Talker is the one that controls the blue light
 	turn_off.set_action_name("on");	// Because 'on' is 'off' on the ESP8266
 
 	// ESP8266 has the HIGH and LOW inverted, so, the LED has to be set as HIG to be turned off
