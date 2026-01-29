de1b8be74c31288f709cc61c6830809877fef5ce
Rui Monteiro <ruiseixas@yahoo.com>
2026-01-29 00:06:19 +0000

    Renamed files

---
+++ JsonTalkie/ESP32_SPI_Length_Master/ESP32_SPI_Length_Master.ino
@@ -0,0 +1,132 @@
+/*
+JsonTalkie - Json Talkie is intended for direct IoT communication.
+Original Copyright (c) 2025 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonTalkie
+*/
+
+#include <Arduino.h>
+#include "driver/spi_master.h"
+
+#define HSPI_CS 15
+#define DATA_SIZE 128
+
+spi_device_handle_t spi;
+uint8_t data_buffer[DATA_SIZE] __attribute__((aligned(4)));
+
+void send1Byte(uint8_t data) {
+    static uint8_t tx_byte __attribute__((aligned(4))) = 0;
+    tx_byte = data;
+    spi_transaction_t t = {};
+    t.length = 8;
+    t.tx_buffer = &tx_byte;
+    t.rx_buffer = nullptr;
+    spi_device_transmit(spi, &t);
+}
+
+uint8_t receive1Byte() {
+    static uint8_t rx_byte __attribute__((aligned(4))) = 0;
+    spi_transaction_t t = {};
+    t.length = 8;
+    t.tx_buffer = nullptr;
+    t.rx_buffer = &rx_byte;
+    spi_device_transmit(spi, &t);
+    return rx_byte;
+}
+
+void send128Bytes() {
+	
+    // DEBUG: Print first 10 bytes before sending
+    Serial.print("send128Bytes() first 10: ");
+    for(int i = 0; i < 10; i++) {
+        Serial.printf("%02X ", data_buffer[i]);
+    }
+    Serial.println();
+
+    spi_transaction_t t = {};
+    t.length = DATA_SIZE * 8;
+    t.tx_buffer = data_buffer;
+    t.rx_buffer = nullptr;
+    spi_device_transmit(spi, &t);
+}
+
+void receive128Bytes() {
+    spi_transaction_t t = {};
+    t.length = DATA_SIZE * 8;
+    t.tx_buffer = nullptr;
+    t.rx_buffer = data_buffer;
+    spi_device_transmit(spi, &t);
+}
+
+void setup() {
+    Serial.begin(115200);
+    delay(2000);
+    randomSeed(analogRead(0));
+    
+    spi_bus_config_t buscfg = {};
+    buscfg.mosi_io_num = 13;
+    buscfg.miso_io_num = 12;
+    buscfg.sclk_io_num = 14;
+    buscfg.quadwp_io_num = -1;
+    buscfg.quadhd_io_num = -1;
+    buscfg.max_transfer_sz = DATA_SIZE;
+    
+    spi_device_interface_config_t devcfg = {};
+    devcfg.clock_speed_hz = 1000000;
+    devcfg.mode = 0;
+    devcfg.spics_io_num = HSPI_CS;
+    devcfg.queue_size = 3;
+    
+    spi_bus_initialize(HSPI_HOST, &buscfg, SPI_DMA_CH_AUTO);
+    spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
+    
+    Serial.println("Master ready");
+}
+
+void loop() {
+    static uint32_t cycle = 0;
+    Serial.printf("\n=== Cycle %lu ===\n", cycle++);
+    
+    if (random(100) < 10) {
+        char temp[128];
+        int len = snprintf(temp, 128, "MasterData_%lu:Value=%ld", millis(), random(10000));
+        if (len > 127) len = 127;
+        
+        memset(data_buffer, 0, DATA_SIZE);
+        memcpy(data_buffer, temp, len);
+        
+        send1Byte(len); // D=0, L=len
+        delayMicroseconds(200);
+        send128Bytes();
+        
+        Serial.printf("[Master] Sent %d bytes\n", len);
+    } else {
+        send1Byte(0b10000000); // D=1, L=0
+        delayMicroseconds(200);
+        
+        uint8_t response = receive1Byte();
+        uint8_t d = (response >> 7) & 0x01;
+        uint8_t l = response & 0x7F;
+        
+        Serial.printf("[Master] Slave: 0x%02X D=%d L=%d\n", response, d, l);
+        
+        if (d == 1 && l > 0) {
+            delayMicroseconds(200);
+            receive128Bytes();
+            Serial.print("Received: ");
+            for (int i = 0; i < l && i < 40; i++) {
+                Serial.print((char)data_buffer[i]);
+            }
+            Serial.println();
+        }
+    }
+    
+    delay(1000);
+}
---
+++ JsonTalkie/ESP32_SPI_Length_Slave/ESP32_SPI_Length_Slave.ino
@@ -0,0 +1,114 @@
+/*
+JsonTalkie - Json Talkie is intended for direct IoT communication.
+Original Copyright (c) 2025 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonTalkie
+*/
+
+#include <Arduino.h>
+#include "driver/spi_slave.h"
+
+#define VSPI_CS 5
+#define DATA_SIZE 128
+
+uint8_t rx_buffer[DATA_SIZE] __attribute__((aligned(4)));
+uint8_t tx_buffer[DATA_SIZE] __attribute__((aligned(4)));
+uint8_t cmd_byte __attribute__((aligned(4)));
+
+void setup() {
+    Serial.begin(115200);
+    delay(2000);
+    
+    spi_bus_config_t buscfg = {};
+    buscfg.mosi_io_num = 23;
+    buscfg.miso_io_num = 19;
+    buscfg.sclk_io_num = 18;
+    buscfg.quadwp_io_num = -1;
+    buscfg.quadhd_io_num = -1;
+    buscfg.max_transfer_sz = DATA_SIZE;
+    
+    spi_slave_interface_config_t slvcfg = {};
+    slvcfg.mode = 0;
+    slvcfg.spics_io_num = VSPI_CS;
+    slvcfg.queue_size = 3;
+    
+    spi_slave_initialize(VSPI_HOST, &buscfg, &slvcfg, SPI_DMA_CH_AUTO);
+    Serial.println("Slave ready");
+}
+
+void loop() {
+    bool slave_has_data = (random(100) < 10);
+    
+    memset(tx_buffer, 0, DATA_SIZE);
+    if (slave_has_data) {
+        snprintf((char*)tx_buffer, DATA_SIZE, "SlaveData_%lu", millis());
+    }
+    
+    spi_slave_transaction_t t1 = {};
+    t1.length = 8;
+    t1.rx_buffer = &cmd_byte;
+    t1.tx_buffer = nullptr;
+    spi_slave_transmit(VSPI_HOST, &t1, portMAX_DELAY);
+    
+    uint8_t d = (cmd_byte >> 7) & 0x01;
+    uint8_t l = cmd_byte & 0x7F;
+    
+    Serial.printf("\n[Slave] Cmd: 0x%02X D=%d L=%d ", cmd_byte, d, l);
+    
+    if (d == 0) {
+        if (l > 0) {
+            Serial.printf("Receiving %d bytes\n", l);
+            delayMicroseconds(100);
+            
+            spi_slave_transaction_t t2 = {};
+            t2.length = DATA_SIZE * 8;
+            t2.rx_buffer = rx_buffer;
+            t2.tx_buffer = nullptr;
+            spi_slave_transmit(VSPI_HOST, &t2, portMAX_DELAY);
+            
+            Serial.print("Received: ");
+            for (int i = 0; i < l && i < 40; i++) {
+                char c = rx_buffer[i];
+                if (c >= 32 && c <= 126) Serial.print(c);
+                else Serial.printf("[%02X]", c);
+            }
+            Serial.println();
+        } else {
+            Serial.println("Master ping");
+        }
+    } else {
+        uint8_t response_byte __attribute__((aligned(4)));
+        int data_len = strlen((char*)tx_buffer);
+        if (data_len > 127) data_len = 127;
+        
+        if (slave_has_data) {
+            response_byte = 0x80 | data_len;
+            Serial.printf("Sending %d bytes\n", data_len);
+        } else {
+            response_byte = 0x80;
+            Serial.println("No data");
+        }
+        
+        spi_slave_transaction_t t2 = {};
+        t2.length = 8;
+        t2.rx_buffer = nullptr;
+        t2.tx_buffer = &response_byte;
+        spi_slave_transmit(VSPI_HOST, &t2, portMAX_DELAY);
+        
+        if (slave_has_data) {
+            delayMicroseconds(100);
+            spi_slave_transaction_t t3 = {};
+            t3.length = DATA_SIZE * 8;
+            t3.rx_buffer = nullptr;
+            t3.tx_buffer = tx_buffer;
+            spi_slave_transmit(VSPI_HOST, &t3, portMAX_DELAY);
+        }
+    }
+}
