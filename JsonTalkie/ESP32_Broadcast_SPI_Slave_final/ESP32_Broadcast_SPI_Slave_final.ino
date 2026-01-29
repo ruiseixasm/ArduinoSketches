5c153f04b566722c657e45cc7b3e5c72ebb49261
Rui Monteiro <ruiseixas@yahoo.com>
2026-01-29 17:49:54 +0000

    Updated the code to use interrupts

--- JsonTalkie/ESP32_Broadcast_SPI_Slave_final/ESP32_Broadcast_SPI_Slave_final.ino
+++ JsonTalkie/ESP32_Broadcast_SPI_Slave_final/ESP32_Broadcast_SPI_Slave_final.ino
@@ -15,108 +15,135 @@ https://github.com/ruiseixasm/JsonTalkie
 #include <Arduino.h>
 #include "driver/spi_slave.h"
 
-#define VSPI_CS 5
+#define VSPI_CS   5
 #define DATA_SIZE 128
-// Convert 3000ms to FreeRTOS ticks
-#define TIMEOUT_MS 3000
 
 uint8_t rx_buffer[DATA_SIZE] __attribute__((aligned(4)));
 uint8_t tx_buffer[DATA_SIZE] __attribute__((aligned(4)));
 uint8_t cmd_byte __attribute__((aligned(4)));
 uint8_t sending_length __attribute__((aligned(4))) = 0;
 
+spi_slave_transaction_t cmd_trans;
+spi_slave_transaction_t data_trans;
+
+enum SpiState {
+    WAIT_CMD,
+    RX_PAYLOAD,
+    TX_PAYLOAD
+};
+
+SpiState spi_state = WAIT_CMD;
+
+void queue_cmd_transaction() {
+    memset(&cmd_trans, 0, sizeof(cmd_trans));
+    cmd_trans.length    = 8;
+    cmd_trans.rx_buffer = &cmd_byte;
+    cmd_trans.tx_buffer = &sending_length;   // always report availability
+    spi_slave_queue_trans(VSPI_HOST, &cmd_trans, portMAX_DELAY);
+}
+
+void queue_rx_transaction(uint8_t len) {
+    memset(&data_trans, 0, sizeof(data_trans));
+    data_trans.length    = len * 8;
+    data_trans.rx_buffer = rx_buffer;
+    spi_slave_queue_trans(VSPI_HOST, &data_trans, portMAX_DELAY);
+}
+
+void queue_tx_transaction(uint8_t len) {
+    memset(&data_trans, 0, sizeof(data_trans));
+    data_trans.length    = len * 8;
+    data_trans.tx_buffer = tx_buffer;
+    spi_slave_queue_trans(VSPI_HOST, &data_trans, portMAX_DELAY);
+}
+
 void setup() {
     Serial.begin(115200);
     delay(2000);
-    
+
     spi_bus_config_t buscfg = {};
     buscfg.mosi_io_num = 23;
     buscfg.miso_io_num = 19;
     buscfg.sclk_io_num = 18;
-    buscfg.quadwp_io_num = -1;
-    buscfg.quadhd_io_num = -1;
     buscfg.max_transfer_sz = DATA_SIZE;
-    
+
     spi_slave_interface_config_t slvcfg = {};
     slvcfg.mode = 0;
     slvcfg.spics_io_num = VSPI_CS;
     slvcfg.queue_size = 3;
-    
+
     spi_slave_initialize(VSPI_HOST, &buscfg, &slvcfg, SPI_DMA_CH_AUTO);
     Serial.println("Slave ready");
-}
 
+    queue_cmd_transaction();   // ARM IMMEDIATELY
+}
 
 void loop() {
-	
-	// Convert milliseconds to FreeRTOS ticks
-	const TickType_t timeout_ticks = TIMEOUT_MS / portTICK_PERIOD_MS;
+    spi_slave_transaction_t *ret;
 
-    if (sending_length == 0) {
-        int data_len = 0;
-        bool slave_has_data = (random(100) < 10);
-        if (slave_has_data) {
-            data_len = snprintf((char*)tx_buffer, DATA_SIZE, "SlaveData_%lu", millis());
-            if (data_len > 127) data_len = 127;
-            sending_length = (uint8_t)data_len;
-        }
+    /* --- generate data opportunistically --- */
+    if (sending_length == 0 && random(100) < 10) {
+        int len = snprintf((char*)tx_buffer, DATA_SIZE,
+                           "SlaveData_%lu", millis());
+        if (len > 127) len = 127;
+        sending_length = (uint8_t)len;
     }
 
-    spi_slave_transaction_t t = {};
-    t.length = 1 * 8;	// Bytes to bits
-    t.rx_buffer = &cmd_byte;
-    t.tx_buffer = &sending_length;  // Always send it's size
-    spi_slave_transmit(VSPI_HOST, &t, timeout_ticks);
-    
+    /* --- check for completed SPI transaction --- */
+    if (spi_slave_get_trans_result(VSPI_HOST, &ret, 0) != ESP_OK) {
+        return; // nothing finished yet
+    }
 
-    bool beacon = (bool)((cmd_byte >> 7) & 0x01);
-    uint8_t received_length = cmd_byte & 0b01111111;
-    if (!beacon) {	// Master sends
-		
-    	Serial.printf("\n[From Master] Cmd: 0x%02X Beacon=0 L=%d ", cmd_byte, received_length);
-    
-        if (received_length > 0) {
-			
-			if (received_length <= DATA_SIZE) {
-	
-				Serial.printf("Receiving %d bytes\n", received_length);
-				delayMicroseconds(100);
-				
-				t.length = received_length * 8;	// Bytes to bits
-				t.rx_buffer = rx_buffer;
-				t.tx_buffer = nullptr;
-				spi_slave_transmit(VSPI_HOST, &t, timeout_ticks);
-				
-				Serial.print("Received: ");
-				for (int i = 0; i < received_length; i++) {
-					char c = rx_buffer[i];
-					if (c >= 32 && c <= 126) Serial.print(c);
-					else Serial.printf("[%02X]", c);
-				}
-				Serial.println();
-			}
+    /* --- "ISR" logic --- */
+    if (spi_state == WAIT_CMD) {
+        bool beacon = (cmd_byte & 0x80);
+        uint8_t received_length = cmd_byte & 0x7F;
+
+        Serial.printf("\n[CMD] 0x%02X beacon=%d len=%u\n",
+                      cmd_byte, beacon, received_length);
+
+        if (!beacon) {
+            if (received_length > 0 && received_length <= DATA_SIZE) {
+                spi_state = RX_PAYLOAD;
+                queue_rx_transaction(received_length);
+                return;
+            } else {
+                Serial.println("Master ping");
+            }
         } else {
-            Serial.println("Master ping");
+            if (received_length > 0) {
+                spi_state = TX_PAYLOAD;
+                queue_tx_transaction(received_length);
+                return;
+            } else {
+                Serial.println("Beacon, no data requested");
+            }
         }
 
-    } else if (received_length > 0) {    // Beacon with length
-        
-        sending_length = 0;    // Payload sent
-        delayMicroseconds(100);
-        t.length = (size_t)received_length * 8;	// Bytes to bits
-        t.rx_buffer = nullptr;
-        t.tx_buffer = tx_buffer;
-        spi_slave_transmit(VSPI_HOST, &t, timeout_ticks);
-		
-    	Serial.printf("\n[To Beacon |2] Cmd: 0x%02X Beacon=1 L=%d ", cmd_byte, received_length);
-        Serial.printf("\n[From Slave] Sending %d bytes\n", received_length);
-    } else {
-		Serial.printf("\n[To Beacon |1] Cmd: 0x%02X Beacon=1 L=%d ", cmd_byte, sending_length);
-		if (sending_length == 0) {
-        	Serial.println("\n[From Slave] No data to be sent");
-		}
-	}
+        queue_cmd_transaction();
+    }
+
+    else if (spi_state == RX_PAYLOAD) {
+        Serial.print("Received: ");
+        for (int i = 0; i < (cmd_byte & 0x7F); i++) {
+            char c = rx_buffer[i];
+            if (c >= 32 && c <= 126) Serial.print(c);
+            else Serial.printf("[%02X]", c);
+        }
+        Serial.println();
+
+        spi_state = WAIT_CMD;
+        queue_cmd_transaction();
+    }
+
+    else if (spi_state == TX_PAYLOAD) {
+        Serial.printf("Sent %u bytes\n", sending_length);
+        sending_length = 0;
+
+        spi_state = WAIT_CMD;
+        queue_cmd_transaction();
+    }
 }
+
 
 
 // // LOGIC TO BE USED BY SPI SLAVES
