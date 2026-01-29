2b82de3250c9e8dee27faae5b3d8308b0d0095e3
Rui Monteiro <ruiseixas@yahoo.com>
2026-01-29 17:29:10 +0000

    Added the final sketches

---
+++ JsonTalkie/ESP32_Broadcast_SPI_Master_final/ESP32_Broadcast_SPI_Master_final.ino
@@ -0,0 +1,179 @@
+/*
+JsonTalkie - Json Talkie is intended for direct IoT communication.
+Original Copyright (c) 2025 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonTalkie
+*/
+
+#include <Arduino.h>
+#include "driver/spi_master.h"
+
+// LED_BUILTIN is already defined by ESP32 platform
+// Typically GPIO2 for most ESP32 boards
+#ifndef LED_BUILTIN
+  #define LED_BUILTIN 2  // Fallback definition if not already defined
+#endif
+
+#define HSPI_CS 15
+#define DATA_SIZE 128
+
+spi_device_handle_t spi;
+uint8_t data_buffer[DATA_SIZE] __attribute__((aligned(4)));
+
+int spi_cs_pins[] = {4, HSPI_CS};
+
+
+void broadcastLength(int* ss_pins, uint8_t ss_pins_count, uint8_t length) {
+    uint8_t tx_byte __attribute__((aligned(4))) = 0b01111111 & length;
+    spi_transaction_t t = {};
+    t.length = 1 * 8;	// Bytes to bits
+    t.tx_buffer = &tx_byte;
+    t.rx_buffer = nullptr;
+
+    for (uint8_t ss_pin_i = 0; ss_pin_i < ss_pins_count; ss_pin_i++) {
+        digitalWrite(ss_pins[ss_pin_i], LOW);
+    }    
+    spi_device_transmit(spi, &t);
+    for (uint8_t ss_pin_i = 0; ss_pin_i < ss_pins_count; ss_pin_i++) {
+        digitalWrite(ss_pins[ss_pin_i], HIGH);
+    }
+}
+
+void broadcastPayload(int* ss_pins, uint8_t ss_pins_count, uint8_t length) {
+
+	if (length > DATA_SIZE) return;
+	
+    Serial.print("broadcastPayload() first 10: ");
+    for(int i = 0; i < length; i++) {
+        Serial.printf("%02X ", data_buffer[i]);
+    }
+    Serial.println();
+
+    spi_transaction_t t = {};
+    t.length = (size_t)length * 8;	// Bytes to bits
+    t.tx_buffer = data_buffer;
+    t.rx_buffer = nullptr;
+
+    for (uint8_t ss_pin_i = 0; ss_pin_i < ss_pins_count; ss_pin_i++) {
+        digitalWrite(ss_pins[ss_pin_i], LOW);
+    }
+    spi_device_transmit(spi, &t);
+    for (uint8_t ss_pin_i = 0; ss_pin_i < ss_pins_count; ss_pin_i++) {
+        digitalWrite(ss_pins[ss_pin_i], HIGH);
+    }
+}
+
+
+uint8_t sendBeacon(int ss_pin, uint8_t length = 0) {
+    uint8_t tx_byte __attribute__((aligned(4))) = 0b10000000 | length;
+    uint8_t rx_byte __attribute__((aligned(4))) = 0;
+    spi_transaction_t t = {};
+    t.length = 1 * 8;	// Bytes to bits
+    t.tx_buffer = &tx_byte;
+    t.rx_buffer = &rx_byte;
+
+    digitalWrite(ss_pin, LOW);
+    spi_device_transmit(spi, &t);
+    digitalWrite(ss_pin, HIGH);
+
+    return rx_byte;
+}
+
+void receivePayload(int ss_pin, uint8_t length = 0) {
+	
+	if (length > DATA_SIZE) return;
+	
+    spi_transaction_t t = {};
+    t.length = (size_t)length * 8;	// Bytes to bits
+    t.tx_buffer = nullptr;
+    t.rx_buffer = data_buffer;
+    
+    digitalWrite(ss_pin, LOW);
+    spi_device_transmit(spi, &t);
+    digitalWrite(ss_pin, HIGH);
+}
+
+
+void setup() {
+    Serial.begin(115200);
+    delay(2000);
+    randomSeed(analogRead(0));
+    
+    spi_bus_config_t buscfg = {};
+    buscfg.mosi_io_num = 13;
+    buscfg.miso_io_num = 12;
+    buscfg.sclk_io_num = 14;
+    buscfg.quadwp_io_num = -1;
+    buscfg.quadhd_io_num = -1;
+    buscfg.max_transfer_sz = DATA_SIZE;
+    
+	// https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/peripherals/spi_master.html
+
+    spi_device_interface_config_t devcfg = {};
+    devcfg.clock_speed_hz = 1000000;
+    devcfg.mode = 0;
+    devcfg.queue_size = 3;
+    devcfg.spics_io_num = -1,  // DISABLE hardware CS completely! (Broadcast)
+    
+    
+    spi_bus_initialize(HSPI_HOST, &buscfg, SPI_DMA_CH_AUTO);
+    spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
+    
+    for (uint8_t ss_pin_i = 0; ss_pin_i < sizeof(spi_cs_pins)/sizeof(int); ss_pin_i++) {
+        pinMode(spi_cs_pins[ss_pin_i], OUTPUT);
+    }
+
+    // Initialize pins FIRST before anything else
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH); // Start with LED on for signalling MASTER
+    
+    Serial.println("Master ready");
+}
+
+
+void loop() {
+    static uint32_t cycle = 0;
+    Serial.printf("\n=== Cycle %lu ===\n", cycle++);
+    
+    if (random(100) < 10) {
+		
+        int len = snprintf((char*)data_buffer, 128, "MasterData_%lu:Value=%ld", millis(), random(10000));
+        if (len > 127) len = 127;
+        
+        Serial.printf("\n[From Master] Slave: 0x%02X Beacon=0 L=%d\n", len, len);
+        broadcastLength(spi_cs_pins, sizeof(spi_cs_pins)/sizeof(int), (uint8_t)len); // D=0, L=len
+        delayMicroseconds(200);
+        broadcastPayload(spi_cs_pins, sizeof(spi_cs_pins)/sizeof(int), (uint8_t)len);
+        
+        Serial.printf("[To Slave] Sent %d bytes\n", len);
+
+    } else {
+        
+        for (uint8_t ss_pin_i = 0; ss_pin_i < sizeof(spi_cs_pins)/sizeof(int); ss_pin_i++) {
+
+			uint8_t l = sendBeacon(spi_cs_pins[ss_pin_i]);
+            Serial.printf("\n[From Beacon to pin %d |1] Slave: 0x%02X Beacon=1 L=%d\n", spi_cs_pins[ss_pin_i], 0b10000000, 0);
+            
+            if (l > 0) {
+                delayMicroseconds(200);
+                sendBeacon(spi_cs_pins[ss_pin_i], l);
+            	Serial.printf("[From Beacon to pin %d |2] Slave: 0x%02X Beacon=1 L=%d\n", spi_cs_pins[ss_pin_i], 0b10000000 | l, l);
+                delayMicroseconds(200);
+                receivePayload(spi_cs_pins[ss_pin_i], l);
+                Serial.print("[From Slave] Received: ");
+                for (int i = 0; i < l; i++) {
+                    Serial.print((char)data_buffer[i]);
+                }
+                Serial.println();
+            }
+        }
+    }
+    delay(1000);
+}
---
+++ JsonTalkie/ESP32_Broadcast_SPI_Slave_final/ESP32_Broadcast_SPI_Slave_final.ino
@@ -0,0 +1,132 @@
+/*
+JsonTalkie - Json Talkie is intended for direct IoT communication.
+Original Copyright (c) 2025 Rui Seixas Monteiro. All right reserved.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+https://github.com/ruiseixasm/JsonTalkie
+*/
+
+#include <Arduino.h>
+#include "driver/spi_slave.h"
+
+#define VSPI_CS 5
+#define DATA_SIZE 128
+// Convert 3000ms to FreeRTOS ticks
+#define TIMEOUT_MS 3000
+
+uint8_t rx_buffer[DATA_SIZE] __attribute__((aligned(4)));
+uint8_t tx_buffer[DATA_SIZE] __attribute__((aligned(4)));
+uint8_t cmd_byte __attribute__((aligned(4)));
+uint8_t sending_length __attribute__((aligned(4))) = 0;
+
+void setup() {
+    Serial.begin(115200);
+    delay(2000);
+    
+    spi_bus_config_t buscfg = {};
+    buscfg.mosi_io_num = 23;
+    buscfg.miso_io_num = 19;
+    buscfg.sclk_io_num = 18;
+    buscfg.quadwp_io_num = -1;
+    buscfg.quadhd_io_num = -1;
+    buscfg.max_transfer_sz = DATA_SIZE;
+    
+    spi_slave_interface_config_t slvcfg = {};
+    slvcfg.mode = 0;
+    slvcfg.spics_io_num = VSPI_CS;
+    slvcfg.queue_size = 3;
+    
+    spi_slave_initialize(VSPI_HOST, &buscfg, &slvcfg, SPI_DMA_CH_AUTO);
+    Serial.println("Slave ready");
+}
+
+
+void loop() {
+	
+	// Convert milliseconds to FreeRTOS ticks
+	const TickType_t timeout_ticks = TIMEOUT_MS / portTICK_PERIOD_MS;
+
+    if (sending_length == 0) {
+        int data_len = 0;
+        bool slave_has_data = (random(100) < 10);
+        if (slave_has_data) {
+            data_len = snprintf((char*)tx_buffer, DATA_SIZE, "SlaveData_%lu", millis());
+            if (data_len > 127) data_len = 127;
+            sending_length = (uint8_t)data_len;
+        }
+    }
+
+    spi_slave_transaction_t t = {};
+    t.length = 1 * 8;	// Bytes to bits
+    t.rx_buffer = &cmd_byte;
+    t.tx_buffer = &sending_length;  // Always send it's size
+    spi_slave_transmit(VSPI_HOST, &t, timeout_ticks);
+    
+
+    bool beacon = (bool)((cmd_byte >> 7) & 0x01);
+    uint8_t received_length = cmd_byte & 0b01111111;
+    if (!beacon) {	// Master sends
+		
+    	Serial.printf("\n[From Master] Cmd: 0x%02X Beacon=0 L=%d ", cmd_byte, received_length);
+    
+        if (received_length > 0) {
+			
+			if (received_length <= DATA_SIZE) {
+	
+				Serial.printf("Receiving %d bytes\n", received_length);
+				delayMicroseconds(100);
+				
+				t.length = received_length * 8;	// Bytes to bits
+				t.rx_buffer = rx_buffer;
+				t.tx_buffer = nullptr;
+				spi_slave_transmit(VSPI_HOST, &t, timeout_ticks);
+				
+				Serial.print("Received: ");
+				for (int i = 0; i < received_length; i++) {
+					char c = rx_buffer[i];
+					if (c >= 32 && c <= 126) Serial.print(c);
+					else Serial.printf("[%02X]", c);
+				}
+				Serial.println();
+			}
+        } else {
+            Serial.println("Master ping");
+        }
+
+    } else if (received_length > 0) {    // Beacon with length
+        
+        sending_length = 0;    // Payload sent
+        delayMicroseconds(100);
+        t.length = (size_t)received_length * 8;	// Bytes to bits
+        t.rx_buffer = nullptr;
+        t.tx_buffer = tx_buffer;
+        spi_slave_transmit(VSPI_HOST, &t, timeout_ticks);
+		
+    	Serial.printf("\n[To Beacon |2] Cmd: 0x%02X Beacon=1 L=%d ", cmd_byte, received_length);
+        Serial.printf("\n[From Slave] Sending %d bytes\n", received_length);
+    } else {
+		Serial.printf("\n[To Beacon |1] Cmd: 0x%02X Beacon=1 L=%d ", cmd_byte, sending_length);
+		if (sending_length == 0) {
+        	Serial.println("\n[From Slave] No data to be sent");
+		}
+	}
+}
+
+
+// // LOGIC TO BE USED BY SPI SLAVES
+
+// // once
+// queue_idle_transaction();
+
+// // forever
+// if (spi_slave_get_trans_result(host, &t, 0) == ESP_OK) {
+//     process(t);
+//     queue_next_transaction();
+// }
+
