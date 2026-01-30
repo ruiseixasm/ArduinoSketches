af4e19fdcdda1029c018b3612ebfd1e9fb1ae34e
Rui Monteiro <ruiseixas@yahoo.com>
2026-01-30 19:07:59 +0000

    No warnings code

--- JsonTalkie/examples/ESP32_Broadcast_SPI_ESP_Master/S_Broadcast_SPI_2xESP_Master.hpp
+++ JsonTalkie/examples/ESP32_Broadcast_SPI_ESP_Master/S_Broadcast_SPI_2xESP_Master.hpp
@@ -101,7 +101,7 @@ protected:
 						_startTransmission(new_message);
 					}
 				}
-				actual_pin_index = ++actual_pin_index % _ss_pins_count;
+				actual_pin_index = (actual_pin_index + 1) % _ss_pins_count;
 			}
 		}
     }
