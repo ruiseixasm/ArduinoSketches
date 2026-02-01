1c3bfe197b62382b65fe5c683486d6c6f58cfb5b
Rui Monteiro <ruiseixas@yahoo.com>
2026-02-01 16:39:30 +0000

    Improved Slave receiving limit

--- JsonTalkie/examples/ESP32_Broadcast_SPI_ESP_Slave/S_Broadcast_SPI_ESP_Slave.hpp
+++ JsonTalkie/examples/ESP32_Broadcast_SPI_ESP_Slave/S_Broadcast_SPI_ESP_Slave.hpp
@@ -149,12 +149,13 @@ protected:
 							static_cast<size_t>( cmd_length )
 						);
 
+						stacked_transmissions++;	// Limits to 5 if before than queue_cmd(), otherwise limits to 6
+
 						// Needs the queue a new command, otherwise nothing is processed again (lock)
 						// Real scenario if at this moment a payload is still in the queue to be sent and now
 						// has no queue to be picked up
 						queue_cmd();	// After the reading above to avoid _rx_buffer corruption
 						
-						stacked_transmissions++;
 						_startTransmission(new_message);
 						stacked_transmissions--;
 						
